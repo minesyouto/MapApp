16c89287690d0669e191f5ae4c2b38d53b0ab2a3
minesyouto <minesyouto8833@gmail.com>
2022-01-31 11:31:39 +0900

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by みねた on 2022/01/31.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
